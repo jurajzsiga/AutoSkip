7d855f2c3e39f21875426d78a41d34798eb2a393
Juraj Zsiga <zsiga@webscope.io>
2020-12-19 20:29:25 +0100

    first commit

---
+++ netflix.js
@@ -0,0 +1,17 @@
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+const appWrapper = $("#appMountPoint")[0]
+console.log('here', appWrapper)
+
+let observer = new MutationObserver(function (mutation, observer) {
+  $(appWrapper).find("a[aria-label='Skip Intro']").each(function (index) {
+    console.log($(this)[0])
+    $(this)[0].click()
+  })
+})
+
+observer.observe(appWrapper, {
+  subtree: true,
+  attributes: false,
+  childList: true
+})
